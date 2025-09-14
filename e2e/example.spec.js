5babbfbdb4856b868f780b4b85d0716a3db59ac7
Johan Joshua Icogo <jicogo@Johans-MacBook-Pro.local>
2025-09-14 13:59:40 +0800

    add e2e test

--- e2e/example.spec.js
+++ e2e/example.spec.js
@@ -1,9 +1,16 @@
 const { test, describe, expect } = require('@playwright/test')
+const { beforeEach } = require('node:test')
 
 describe('Pokedex', () => {
   test('front page can be opened', async ({ page }) => {
-     await page.goto('/')
-    await expect(page.getByText('ivysaur')).toBeVisible()
+    await page.goto('/')
+    await expect(page.getByText('Ivysaur')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
+  })
+
+  test('should navigate to ivysaur page', async ({ page }) => {
+    await page.goto('/')
+    await page.click('text=Ivysaur')
+    await expect(page).toHaveURL(/.*\/ivysaur/)
   })
 })
